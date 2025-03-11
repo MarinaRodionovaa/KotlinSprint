c75274d0209d1499f4a1dcb1dcfaf5f43b35c060
marina_rodionova <marina_rodionova@>
2025-03-11 22:21:53 +0700

    KS-1-3 Data spaceflight

---
+++ src/main/kotlin/Lesson1/lesson1_task3.kt
@@ -0,0 +1,19 @@
+package org.example.lesson1
+
+
+fun main(){
+    val yearOfFlight: Int = 1961
+    var hour: Int = 9
+    var minute: Int  = 7
+
+    println(yearOfFlight)
+    println(hour)
+    println(minute)
+
+    hour = 10
+    minute = 55
+
+    println("$hour:$minute")
+
+
+}
