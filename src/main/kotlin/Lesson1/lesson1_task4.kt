e5efca6e047ea095a79a68458b393427632122d9
marina_rodionova <marina_rodionova@>
2025-03-11 23:23:57 +0700

    KS-1-4 find the most lightweight data type

---
+++ src/main/kotlin/Lesson1/lesson1_task4.kt
@@ -0,0 +1,15 @@
+package org.example.Lesson1
+
+fun main(){
+
+    val length: Long =  40868600000
+    val age: Byte = 27
+    val partOfDay: Double = 0.075
+    val seconds:Short = 6480
+    val partOfYear : Double = 2.0547945205479453E-4
+    val apogee:Int = 327000
+
+    println("Расстояние: $length\n Возраст: $age\n Часть дня: $partOfDay\n Секунды: $seconds\n " +
+            "Часть года: $partOfYear\n Апогей орбиты: $apogee")
+
+}
