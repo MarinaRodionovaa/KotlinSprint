72fa7d4197e81b273870f0ab5903d9566f1e8c51
marina_rodionova <marina_rodionova@>
2025-03-12 23:00:17 +0700

    KS-2-3 fix mistakes

--- src/main/kotlin/Lesson2/lesson2_task3.kt
+++ src/main/kotlin/Lesson2/lesson2_task3.kt
@@ -1,14 +1,17 @@
 package org.example.Lesson2
 
+const val TIME_UNITS_PER_LEVEL = 60
+
 fun main() {
 
     val departureHour: Int = 9
     val departureMinute: Int = 39
     val minutesInWay: Int = 457
 
-    val arrivalMinute: Int = (departureMinute + (minutesInWay % 60)) % 60
-    val arrivalHour: Int = departureHour + minutesInWay / 60 + (departureMinute + (minutesInWay % 60)) / 60
+    val arrivalMinute: Int = (departureMinute + (minutesInWay % TIME_UNITS_PER_LEVEL)) % TIME_UNITS_PER_LEVEL
+    val arrivalHour: Int =
+        departureHour + minutesInWay / TIME_UNITS_PER_LEVEL + (departureMinute + (minutesInWay % TIME_UNITS_PER_LEVEL)) / TIME_UNITS_PER_LEVEL
 
-    println("Время прибытия поезда: $arrivalHour:$arrivalMinute  ")
+    println("Время прибытия поезда: " + String.format("%02d:%02d", arrivalHour, arrivalMinute))
 
 }
