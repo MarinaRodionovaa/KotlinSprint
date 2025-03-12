6caa21ab2c85f2b828b0e6d8eab94bdbc7e0806f
marina_rodionova <marina_rodionova@>
2025-03-12 22:54:37 +0700

    KS-1-5 fix mistakes

--- src/main/kotlin/Lesson1/lesson1_task5.kt
+++ src/main/kotlin/Lesson1/lesson1_task5.kt
@@ -1,13 +1,14 @@
 package org.example.Lesson1
 
+const val TIME_UNITS_PER_LEVEL = 60
+
 fun main() {
 
-    val seconds: Int = 6480
+    var seconds: Int = 6480
 
-    val minutes: String = ("0" + (seconds / 60 % 60).toString()).takeLast(2)
-    val hours: String = ("0" + (seconds / 60 / 60).toString()).takeLast(2)
-    val stringSeconds: String = ("0" + (seconds % 60).toString()).takeLast(2)
+    val minutes: Int = seconds / TIME_UNITS_PER_LEVEL % TIME_UNITS_PER_LEVEL
+    val hours: Int = seconds / TIME_UNITS_PER_LEVEL / TIME_UNITS_PER_LEVEL
+    seconds = seconds % TIME_UNITS_PER_LEVEL
 
-
-    println("$hours:$minutes:$stringSeconds")
+    println(String.format("%02d:%02d:%02d", hours, minutes, seconds))
 }
