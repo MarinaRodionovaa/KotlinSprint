bc70d9f9f13d95842b713e0c0b4b9b9948adafea
marina_rodionova <marina_rodionova@>
2025-03-12 20:43:39 +0700

    KS-1-3 Data spaceflight - corrections

--- src/main/kotlin/Lesson1/lesson1_task3.kt
+++ src/main/kotlin/Lesson1/lesson1_task3.kt
@@ -1,19 +1,17 @@
 package org.example.lesson1
 
 
-fun main(){
-    val yearOfFlight: Int = 1961
-    var hour: Int = 9
-    var minute: Int  = 7
+fun main() {
+    val yearOfFlight: String = "1961"
+    var hour: String = "09"
+    var minute: String = "07"
 
     println(yearOfFlight)
     println(hour)
     println(minute)
 
-    hour = 10
-    minute = 55
+    hour = "10"
+    minute = "55"
 
     println("$hour:$minute")
-
-
 }
