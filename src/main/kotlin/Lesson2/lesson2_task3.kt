0384708536c5671eb45f1dfe1d8875ab1d5b7fed
marina_rodionova <marina_rodionova@>
2025-03-12 20:47:15 +0700

    KS-2-3 edit format

--- src/main/kotlin/Lesson2/lesson2_task3.kt
+++ src/main/kotlin/Lesson2/lesson2_task3.kt
@@ -1,14 +1,12 @@
 package org.example.Lesson2
 
-fun main(){
+fun main() {
 
     val departureHour: Int = 9
     val departureMinute: Int = 39
-
     val minutesInWay: Int = 457
 
-    val arrivalMinute : Int = (departureMinute + (minutesInWay % 60)) % 60
-
+    val arrivalMinute: Int = (departureMinute + (minutesInWay % 60)) % 60
     val arrivalHour: Int = departureHour + minutesInWay / 60 + (departureMinute + (minutesInWay % 60)) / 60
 
     println("Время прибытия поезда: $arrivalHour:$arrivalMinute  ")
