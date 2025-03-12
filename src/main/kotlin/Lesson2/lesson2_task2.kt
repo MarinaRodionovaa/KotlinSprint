813dd3da2f211a4cf225bc7bbb0e5d3c963996c3
marina_rodionova <marina_rodionova@>
2025-03-12 18:11:55 +0700

    KS-2-2 costing of accounting

---
+++ src/main/kotlin/Lesson2/lesson2_task2.kt
@@ -0,0 +1,19 @@
+package org.example.Lesson2
+
+fun main(){
+
+    val numbersOfEmployers:Int =  50
+    val numbersOfIntern:Int =  30
+
+    val usualSalary: Int = 30000
+    val internSalary: Int = 20000
+
+    val expenseUsualSalary:Int = usualSalary * numbersOfEmployers
+    val expense:Int = usualSalary * numbersOfEmployers + internSalary * numbersOfIntern
+    val meanSalary: Int =  expense / (numbersOfEmployers + numbersOfIntern)
+
+    println("Расходы на выплату зарплаты постоянных сотрудников: $expenseUsualSalary\n" +
+            "Общие расходы по ЗП после прихода стажеров: $expense\n" +
+            "Средняя ЗП одного сотрудника после устройства стажеров: $meanSalary")
+
+}
