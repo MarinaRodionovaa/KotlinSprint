584f5ed5ab83f1946f00de06662f0956a49451c0
marina_rodionova <marina_rodionova@>
2025-03-12 20:45:15 +0700

    KS-1-5 edit format

--- src/main/kotlin/Lesson1/lesson1_task5.kt
+++ src/main/kotlin/Lesson1/lesson1_task5.kt
@@ -4,9 +4,9 @@ fun main() {
 
     val seconds: Int = 6480
 
-    val minutes: String = ("0"+(seconds / 60 % 60).toString()).takeLast(2)
-    val hours: String = ("0"+(seconds / 60 / 60).toString()).takeLast(2)
-    val stringSeconds: String = ("0"+(seconds % 60).toString()).takeLast(2)
+    val minutes: String = ("0" + (seconds / 60 % 60).toString()).takeLast(2)
+    val hours: String = ("0" + (seconds / 60 / 60).toString()).takeLast(2)
+    val stringSeconds: String = ("0" + (seconds % 60).toString()).takeLast(2)
 
 
     println("$hours:$minutes:$stringSeconds")
