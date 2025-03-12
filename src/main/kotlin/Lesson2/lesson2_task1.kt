5cdb51ab7a28805709ccefb64fb79420aa379110
marina_rodionova <marina_rodionova@>
2025-03-12 23:01:44 +0700

    KS-2-1 fix mistakes

--- src/main/kotlin/Lesson2/lesson2_task1.kt
+++ src/main/kotlin/Lesson2/lesson2_task1.kt
@@ -2,6 +2,5 @@ package org.example.Lesson2
 
 fun main() {
     val marks: List<Double> = listOf(3.0, 4.0, 3.0, 5.0)
-//    val mean: Double = (3.0 + 4.0 + 3.0 + 5.0)/4 -- второй вариант решения, но думаю делать списком оптимальнее
     println("Средняя оценка в классе: " + marks.sum() / marks.count())
 }
