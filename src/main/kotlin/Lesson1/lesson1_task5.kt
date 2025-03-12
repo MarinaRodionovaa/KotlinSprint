a5498aa41ee4b97a93edd6bdedc2f7b334c9f33c
marina_rodionova <marina_rodionova@>
2025-03-12 14:26:31 +0700

    KS-1-5 converting seconds to hours and minutes

--- src/main/kotlin/Lesson1/lesson1_task5.kt
+++ src/main/kotlin/Lesson1/lesson1_task5.kt
@@ -2,11 +2,11 @@ package org.example.Lesson1
 
 fun main() {
 
-    val seconds = 6480
+    val seconds: Int = 6480
 
-    val minutes = ("0"+(seconds / 60 % 60).toString()).takeLast(2)
-    val hours = ("0"+(seconds / 60 / 60).toString()).takeLast(2)
-    val stringSeconds = ("0"+(seconds % 60).toString()).takeLast(2)
+    val minutes: String = ("0"+(seconds / 60 % 60).toString()).takeLast(2)
+    val hours: String = ("0"+(seconds / 60 / 60).toString()).takeLast(2)
+    val stringSeconds: String = ("0"+(seconds % 60).toString()).takeLast(2)
 
 
     println("$hours:$minutes:$stringSeconds")
