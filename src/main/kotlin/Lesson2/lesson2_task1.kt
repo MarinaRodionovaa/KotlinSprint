5843b02cc93a9282101df0c1ac56292c90a1189d
marina_rodionova <marina_rodionova@>
2025-03-12 17:46:54 +0700

    KS-2-1 mean mark

---
+++ src/main/kotlin/Lesson2/lesson2_task1.kt
@@ -0,0 +1,9 @@
+package org.example.Lesson2
+
+fun main(){
+    val marks: List<Double> = listOf(3.0, 4.0, 3.0, 5.0)
+//    val mean: Double = (3.0 + 4.0 + 3.0 + 5.0)/4 -- второй вариант решения, но думаю делать списком оптимальнее
+    println("Средняя оценка в классе: " + marks.sum()/marks.count())
+}
+
+
