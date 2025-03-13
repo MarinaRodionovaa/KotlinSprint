ca2d0b268365bd3f995b6ce6f66e23652fc9fda9
marina_rodionova <marina_rodionova@>
2025-03-13 22:06:56 +0700

    KS-3-4 chess moves

---
+++ src/main/kotlin/Lesson3/lesson3_task4.kt
@@ -0,0 +1,18 @@
+package org.example.Lesson3
+
+fun main() {
+    var chessMoveFrom = "E2"
+    var chessMoveTo = "E4"
+    var chessMoveNumber = 1
+
+    println("[$chessMoveFrom-$chessMoveTo;$chessMoveNumber]")
+
+    chessMoveFrom = "D2"
+    val letterTo = chessMoveFrom.replace(Regex("[^A-Z]"), "")
+    val numberTo = chessMoveFrom.replace(Regex("[^0-9]"), "").toInt() + 1
+    chessMoveTo = letterTo + numberTo
+    chessMoveNumber += 1
+
+    println("[$chessMoveFrom-$chessMoveTo;$chessMoveNumber]")
+
+}
