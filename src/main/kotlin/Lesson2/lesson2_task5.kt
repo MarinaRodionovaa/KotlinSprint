f6c92da51bfb844c74eca67fe7d3dd622957c90b
marina_rodionova <marina_rodionova@>
2025-03-14 16:33:53 +0700

    KS-2-5 solution optimization

--- src/main/kotlin/Lesson2/lesson2_task5.kt
+++ src/main/kotlin/Lesson2/lesson2_task5.kt
@@ -2,17 +2,16 @@ package org.example.Lesson2
 
 const val MAX_PERCENT = 100
 
+fun Int.pow(years: Int, deposit: Double, depositPercent: Double): Double {
+    var depositFinal = deposit
+    repeat(years) { depositFinal += depositFinal * (depositPercent / MAX_PERCENT) }
+    return depositFinal
+}
+
 fun main() {
-    val deposit = 70000.0
-    val depositPercent = 16.7
-    val depositPeriodYears = 20
-    var depositFinal: Double = deposit
+    var depositFinal: Double= 1.pow(20, 70000.0, 16.7)
 
-    for (i in 1..depositPeriodYears) {
-
-        depositFinal += depositFinal * (depositPercent / MAX_PERCENT)
-
-    }
-    println("Размер вклада через 20 лет: ${String.format("%.3f", depositFinal)}")
+    println("Размер вклада через 20 лет: ${String.format("%.3f",depositFinal )}")
 
 }
+
