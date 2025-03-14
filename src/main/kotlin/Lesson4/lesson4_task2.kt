de7c5bcffab9ac7ea38623d207bb990eb86fb8ad
marina_rodionova <marina_rodionova@>
2025-03-14 20:38:58 +0700

    KS-4-2 checking cargo weight

---
+++ src/main/kotlin/Lesson4/lesson4_task2.kt
@@ -0,0 +1,29 @@
+package org.example.Lesson4
+
+fun main() {
+
+    var checkedWeight = 20
+    var checkedVolume = 0
+
+    checkCargo(checkedWeight,checkedVolume)
+
+    checkedWeight = 50
+    checkedVolume = 80
+
+    checkCargo(checkedWeight,checkedVolume)
+
+    checkedWeight = 0
+    checkedVolume = 100
+
+    checkCargo(checkedWeight,checkedVolume)
+}
+
+fun checkCargo(checkedWeight: Int, checkedVolume: Int) {
+    val weightFrom = 35
+    val weightUp = 100
+    val volumeUp = 100
+
+    return println("Груз с весом $checkedWeight кг и объемом $checkedVolume л соответствует категории 'Average':" +
+            " ${weightFrom < checkedWeight && checkedWeight <= weightUp && checkedVolume < volumeUp}")
+
+}
