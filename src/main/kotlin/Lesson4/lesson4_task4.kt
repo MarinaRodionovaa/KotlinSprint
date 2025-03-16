e65db6cde85a6799c359742ae7f064b023d23430
marina_rodionova <marina_rodionova@>
2025-03-16 14:52:37 +0700

    KS-4-4 fix mistakes

--- src/main/kotlin/Lesson4/lesson4_task4.kt
+++ src/main/kotlin/Lesson4/lesson4_task4.kt
@@ -2,11 +2,14 @@ package org.example.Lesson4
 
 fun main() {
     val dayOfExercises = 5
+    val evenDay = dayOfExercises % 2 == 0
+    val armAndAbsDay = !evenDay
+    val LegAndBackDay = evenDay
 
     println(
-        "Упражнения для рук: ${dayOfExercises % 2 != 0}\n" +
-                "Упражнения для ног: ${dayOfExercises % 2 == 0}\n" +
-                "Упражнения для спины: ${dayOfExercises % 2 == 0}\n" +
-                "Упражнения для пресса: ${dayOfExercises % 2 != 0}\n"
+        "Упражнения для рук: $armAndAbsDay\n" +
+                "Упражнения для ног: $LegAndBackDay\n" +
+                "Упражнения для спины: $LegAndBackDay\n" +
+                "Упражнения для пресса: $armAndAbsDay\n"
     )
 }
