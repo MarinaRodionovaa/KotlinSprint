bb17a983970c68aa54f06768d3a699c0b620d218
marina_rodionova <marina_rodionova@>
2025-03-17 18:38:49 +0700

    KS-5-1 authorization check

---
+++ src/main/kotlin/Lesson5/lesson5_task1.kt
@@ -0,0 +1,16 @@
+package org.example.Lesson5
+
+const val FIRST_NUMBER = 8
+const val SECOND_NUMBER = 4
+
+fun main() {
+
+    println("Для авторизации необходимо пройти проверку на бота\nРешите пример\n$FIRST_NUMBER + $SECOND_NUMBER = ")
+    val userAnswer = readLine()?.toIntOrNull() ?: 0
+
+    if (userAnswer == FIRST_NUMBER + SECOND_NUMBER) {
+        println("Добро пожаловать!")
+    } else {
+        println("Доступ запрещен.")
+    }
+}
