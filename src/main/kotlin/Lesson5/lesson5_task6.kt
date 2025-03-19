c4720cc40821348cf28ae95b87bd1c08176afb01
marina_rodionova <marina_rodionova@>
2025-03-19 18:22:34 +0700

    KS-5-6 BMI calculator

--- src/main/kotlin/Lesson5/lesson5_task6.kt
+++ src/main/kotlin/Lesson5/lesson5_task6.kt
@@ -3,13 +3,14 @@ package org.example.Lesson5
 const val FROM_NORMAL_BMI = 18.5
 const val UP_NORMAL_BMI = 25.0
 const val UP_EXTRA_BMI = 30.0
+const val CM_TO_METERS = 100
 
 fun main() {
     println("Введите вес (кг)")
     val userWeight = readLine()?.toDoubleOrNull() ?: -1.0
 
     println("Введите вес (см)")
-    val userHeight = (readLine()?.toDoubleOrNull() ?: -1.0) / 100
+    val userHeight = (readLine()?.toDoubleOrNull() ?: -1.0) / CM_TO_METERS
 
     val userImt = userWeight / (userHeight * userHeight)
     val result = when {
