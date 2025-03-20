80646cd4747b5f8d168c6dc4afe831aee7ad12b2
marina_rodionova <marina_rodionova@>
2025-03-20 15:20:09 +0700

    KS-5-7 fuel calculator fix mistakes

--- src/main/kotlin/Lesson5/lesson5_task7.kt
+++ src/main/kotlin/Lesson5/lesson5_task7.kt
@@ -1,15 +1,17 @@
 package org.example.Lesson5
 
+const val STUB_FOR_NULL = 0.0
+
 fun main() {
     println("Введите следующие данные")
     println("Расстояние поездки (в километрах)")
-    val travelDistance = readLine()?.toDoubleOrNull() ?: 0.0
+    val travelDistance = readln().toDoubleOrNull() ?: STUB_FOR_NULL
 
     println("Расход топлива на 100 км (в литрах)")
-    val fuelСonsumption = (readLine()?.toDoubleOrNull() ?: 0.0)
+    val fuelСonsumption = (readln().toDoubleOrNull() ?: STUB_FOR_NULL)
 
     println("Текущую цену за литр топлива")
-    val fuelPriceForL = (readLine()?.toDoubleOrNull() ?: 0.0)
+    val fuelPriceForL = (readln().toDoubleOrNull() ?: STUB_FOR_NULL)
 
     val totalFuel = (travelDistance * fuelСonsumption) / 100
     val totalFuelPrice = totalFuel * fuelPriceForL
