17774dec23b47d39cd7e1c4795f9a262f027ba64
marina_rodionova <marina_rodionova@>
2025-03-23 01:16:35 +0700

    KS-7-4 timer

---
+++ src/main/kotlin/Lesson7/lesson7_task4.kt
@@ -0,0 +1,16 @@
+package org.example.Lesson7
+
+const val MILLISECONDS_IN_SECOND = 1000L
+const val STUB_NULL = 0
+
+fun main() {
+    println("Введите количество секунд, которые нужно засечь")
+    val userSeconds = readln().toIntOrNull() ?: STUB_NULL
+
+    for (i in userSeconds downTo 1) {
+        println("Осталось $i секунд")
+        Thread.sleep(MILLISECONDS_IN_SECOND)
+    }
+
+    println("Время вышло")
+}
