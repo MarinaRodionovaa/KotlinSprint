8729e512376f6f1fcee9c5b82b0c1a2f5019c9ba
marina_rodionova <marina_rodionova@>
2025-03-22 16:53:53 +0700

    KS-5-7 fix mistakes

--- src/main/kotlin/Lesson5/lesson5_task7.kt
+++ src/main/kotlin/Lesson5/lesson5_task7.kt
@@ -1,17 +1,17 @@
 package org.example.Lesson5
 
-const val STUB_FOR_NULL = 0.0
+const val STUB_NULL = 0.0
 
 fun main() {
     println("Введите следующие данные")
     println("Расстояние поездки (в километрах)")
-    val travelDistance = readln().toDoubleOrNull() ?: STUB_FOR_NULL
+    val travelDistance = readln().toDoubleOrNull() ?: STUB_NULL
 
     println("Расход топлива на 100 км (в литрах)")
-    val fuelСonsumption = (readln().toDoubleOrNull() ?: STUB_FOR_NULL)
+    val fuelСonsumption = (readln().toDoubleOrNull() ?: STUB_NULL)
 
     println("Текущую цену за литр топлива")
-    val fuelPriceForL = (readln().toDoubleOrNull() ?: STUB_FOR_NULL)
+    val fuelPriceForL = (readln().toDoubleOrNull() ?: STUB_NULL)
 
     val totalFuel = (travelDistance * fuelСonsumption) / 100
     val totalFuelPrice = totalFuel * fuelPriceForL
