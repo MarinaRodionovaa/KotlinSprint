f284596ffef526bba12dfbb187b2d260872023c7
marina_rodionova <marina_rodionova@>
2025-03-23 01:02:34 +0700

    KS-7-3 even numbers

--- src/main/kotlin/Lesson7/lesson7_task3.kt
+++ src/main/kotlin/Lesson7/lesson7_task3.kt
@@ -5,8 +5,8 @@ const val STUB_NULL = -1
 fun main() {
     println("Введите число")
     val rangeUp = readln().toIntOrNull() ?: STUB_NULL
-    val range = 0..rangeUp step 2
-    for (i in range) {
+    val rangeEvenNumbers = 0..rangeUp step 2
+    for (i in rangeEvenNumbers) {
         println(i)
     }
 }
