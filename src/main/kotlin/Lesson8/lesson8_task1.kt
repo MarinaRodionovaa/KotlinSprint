e15c78b3042700a74d1a20874f16fd719e2b6a76
marina_rodionova <marina_rodionova@>
2025-03-23 02:54:40 +0700

    KS-8-1 views counter

---
+++ src/main/kotlin/Lesson8/lesson8_task1.kt
@@ -0,0 +1,9 @@
+package org.example.Lesson8
+
+const val DAYS_IN_WEEK = 7
+
+fun main() {
+    val viewsRange = 100..1000
+    val viewsInWeek: Array<Int> = Array(DAYS_IN_WEEK, { viewsRange.random() })
+    println("Сумма просмотров за неделю ${viewsInWeek.sum()}")
+}
