5a040720b923d845445dc62cd93babefbeb77643
marina_rodionova <marina_rodionova@>
2025-03-22 16:55:27 +0700

    KS-6-1 registration authorization

---
+++ src/main/kotlin/Lesson6/lesson6_task1.kt
@@ -0,0 +1,23 @@
+package org.example.Lesson6
+
+fun main() {
+    println("Пройдите регистрацию")
+    println("Придумайте логин")
+    val userLogin = readln()
+    println("Придумайте пароль")
+    val userPass = readln()
+    println("Вы успешно зарегистрированы\nПройдите авторизацию")
+
+    var enteredLogin: String
+    var enteredPass: String
+
+    do {
+        println("Введите логин")
+        enteredLogin = readln()
+        println("Введите пароль")
+        enteredPass = readln()
+    } while (enteredLogin != userLogin && enteredPass != userPass)
+
+    println("Авторизация прошла успешно")
+
+}
