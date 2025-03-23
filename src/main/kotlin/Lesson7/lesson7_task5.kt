af00c69f4add461130d08bf448f25d815838b22b
marina_rodionova <marina_rodionova@>
2025-03-23 19:54:08 +0700

    KS-7-5 password generator fix mistakes

--- src/main/kotlin/Lesson7/lesson7_task5.kt
+++ src/main/kotlin/Lesson7/lesson7_task5.kt
@@ -4,20 +4,23 @@ const val MIN_VALUE = 6
 
 fun main() {
     val lowLetterRange = 'a'..'z'
-    val UpLetterRange = 'A'..'Z'
+    val upLetterRange = 'A'..'Z'
     val numberRange = '0'..'9'
-
-    val rangeList = listOf(UpLetterRange, lowLetterRange, numberRange)
+    val rangeOfCharacters = lowLetterRange + upLetterRange + numberRange
 
     println("Введите кол-во символов в пароле")
     val input = readln().toIntOrNull() ?: MIN_VALUE
     val lengthPassword = if (input < MIN_VALUE) MIN_VALUE else input
 
-    var password = ""
+    val password: MutableList<Char> = mutableListOf()
+    password.add(lowLetterRange.random())
+    password.add(upLetterRange.random())
+    password.add(numberRange.random())
 
-    for (i in 1..lengthPassword) {
-        password += (rangeList.random()).random()
+    for (i in 1..(lengthPassword - password.size)) {
+        password.add(rangeOfCharacters.random())
     }
-    println("Сгенерированный пароль: $password")
+    password.shuffle()
+    println("Сгенерированный пароль: ${password.joinToString("")}")
 
 }
