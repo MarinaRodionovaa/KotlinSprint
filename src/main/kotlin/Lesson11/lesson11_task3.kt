60ae9cb1227f83dbab1f6fbb0cc70b34f0f467d5
marina_rodionova <marina_rodionova@>
2025-03-25 18:28:10 +0700

    KS-11-3 room class

---
+++ src/main/kotlin/Lesson11/lesson11_task3.kt
@@ -0,0 +1,30 @@
+package org.example.Lesson11
+
+fun main() {
+    val user1 = User2("admin", "avatar")
+    val room1 =  Room("Комната", "Первая комната")
+
+    room1.addUser(user1)
+
+    room1.changeStatus(user1, "пользователь заглушен")
+
+}
+class Room(image : String, roomName : String, ) {
+    var userList = mutableMapOf<User2, String>()
+
+    fun addUser(user : User2) {
+        userList.put(user, "микрофон выключен")
+    }
+
+    fun changeStatus(user : User2, status : String) {
+        userList[user] = status
+    }
+
+
+}
+
+
+class User2( val login: String, var avatar: String) {
+
+    }
+
