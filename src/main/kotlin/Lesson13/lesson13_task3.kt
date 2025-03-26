2eab316bfa77a5daec93766e5ab353ff75d6bbad
marina_rodionova <marina_rodionova@>
2025-03-26 23:38:45 +0700

    KS-13-3 phone book list

--- src/main/kotlin/Lesson13/lesson13_task3.kt
+++ src/main/kotlin/Lesson13/lesson13_task3.kt
@@ -3,11 +3,13 @@ package org.example.Lesson13
 const val STUB_NULL = "<не указано>"
 
 fun main() {
-    val phoneBook = listOf(PhoneBook2("Ростислав", 89123456789, null),
+    val phoneBook = listOf(
+        PhoneBook2("Ростислав", 89123456789, null),
         PhoneBook2("Андрей", 89123456799, null),
-        PhoneBook2("Алина", 89003456700, "null"))
+        PhoneBook2("Алина", 89003456700, "null")
+    )
 
-    phoneBook.forEach { println(it.company?: STUB_NULL) }
+    phoneBook.forEach { println(it.company ?: STUB_NULL) }
 }
 
 class PhoneBook2(val name: String, val phone: Long, var company: String?) {
