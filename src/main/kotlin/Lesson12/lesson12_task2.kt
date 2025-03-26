ca728764355a6a531297f55245a493b9dd39a5a6
marina_rodionova <marina_rodionova@>
2025-03-26 19:10:41 +0700

    KS-12-2 whether class constructor

--- src/main/kotlin/Lesson12/lesson12_task2.kt
+++ src/main/kotlin/Lesson12/lesson12_task2.kt
@@ -2,15 +2,15 @@ package org.example.Lesson12
 
 fun main() {
 
-    val day1 = WhetherOnDay(35, 23, false)
+    val day1 = WhetherOnDay1(35, 23, false)
     day1.printInfo()
 
-    val day2 = WhetherOnDay(24, 15, true)
+    val day2 = WhetherOnDay1(24, 15, true)
     day2.printInfo()
 
 }
 
-class WhetherOnDay(val dayDegree: Int, val nightDegree: Int, val isRaining: Boolean) {
+class WhetherOnDay1(val dayDegree: Int, val nightDegree: Int, val isRaining: Boolean) {
 
     fun printInfo() {
         println("Температура днем $dayDegree\nТемпература ночью $nightDegree\nНаличие дождя $isRaining")
