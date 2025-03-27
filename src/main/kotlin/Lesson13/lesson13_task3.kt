1d8b63be8c4fdf2fc8d2717cc65bc4e6a67e69fc
marina_rodionova <marina_rodionova@>
2025-03-27 17:52:09 +0700

    KS-13-3 Phone Book 3

--- src/main/kotlin/Lesson13/lesson13_task3.kt
+++ src/main/kotlin/Lesson13/lesson13_task3.kt
@@ -4,15 +4,15 @@ const val STUB_NULL = "<не указано>"
 
 fun main() {
     val phoneBook = listOf(
-        PhoneBook2("Ростислав", 89123456789, null),
-        PhoneBook2("Андрей", 89123456799, null),
-        PhoneBook2("Алина", 89003456700, "null")
+        PhoneBook3("Ростислав", 89123456789, null),
+        PhoneBook3("Андрей", 89123456799, null),
+        PhoneBook3("Алина", 89003456700, "null")
     )
 
     phoneBook.forEach { println(it.company ?: STUB_NULL) }
 }
 
-class PhoneBook2(val name: String, val phone: Long, var company: String?) {
+class PhoneBook3(val name: String, val phone: Long, var company: String?) {
 
     fun printInfo() {
         println("Имя:${this.name} Номер:${this.phone} Компания: ${this.company ?: STUB_NULL}")
