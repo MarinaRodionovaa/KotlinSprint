38c67ae23a92fe92499085e1dd93b8a2a792504f
marina_rodionova <marina_rodionova@>
2025-03-28 16:16:57 +0700

    change folders

--- src/main/kotlin/Lesson1/lesson1_task1.kt
+++
@@ -1,6 +0,0 @@
-package org.example.Lesson1
-
-fun main(){
-    var countOfOrders: Int = 75
-    val gratitudeText: String = "Спасибо за покупку!"
-}
--- src/main/kotlin/Lesson2/lesson2_task1.kt
+++
@@ -1,6 +0,0 @@
-package org.example.Lesson2
-
-fun main() {
-    val marks: List<Double> = listOf(3.0, 4.0, 3.0, 5.0)
-    println("Средняя оценка в классе: " + marks.sum() / marks.count())
-}
