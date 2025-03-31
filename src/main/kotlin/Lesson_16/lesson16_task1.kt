f01824a4ff936621746a114891df80e3463971d5
marina_rodionova <marina_rodionova@>
2025-03-31 20:48:28 +0700

    KS-16-1 Dice

---
+++ src/main/kotlin/Lesson_16/lesson16_task1.kt
@@ -0,0 +1,16 @@
+package org.example.Lesson_16
+
+fun main() {
+    val dice = Dice()
+    dice.printValue()
+}
+
+private class Dice {
+    private val range = 1..6
+    private val count = range.random()
+
+
+    fun printValue() {
+        println(count)
+    }
+}
