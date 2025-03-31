13eaf383348b6ffcff10063fa4c49c7ce0a8a0d0
marina_rodionova <marina_rodionova@>
2025-03-31 23:43:39 +0700

    KS-16-3 circle

---
+++ src/main/kotlin/Lesson_16/lesson16_task2.kt
@@ -0,0 +1,14 @@
+package org.example.Lesson_16
+
+fun main() {
+    val circle = Circle(12.0)
+    println(circle.perimeter().toInt())
+    println(circle.square().toInt())
+}
+
+class Circle(private val radius: Double) {
+    private val pi = 3.14
+
+    fun square() = radius * (pi * pi)
+    fun perimeter() = radius * 2 * pi
+}
