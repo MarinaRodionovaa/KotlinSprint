27e93fecacfb2a754d78ff32f3c5d94d2cd11840
marina_rodionova <marina_rodionova@>
2025-04-01 17:41:07 +0700

    KS-15-5 fix mistakes

--- src/main/kotlin/Lesson_15/lesson15_task5.kt
+++ src/main/kotlin/Lesson_15/lesson15_task5.kt
@@ -15,6 +15,11 @@ fun main() {
     car1.loadPassengers(2)
     car1.move()
 
+    truck.uploadPassengers()
+    truck.uploadCargo()
+    car.uploadPassengers()
+    car1.uploadPassengers()
+
 }
 
 
@@ -28,28 +33,16 @@ interface PassengerTransportationAble {
     var countOfPassengers: Int
     val maxCountOfPassengers: Int
 
+    fun loadPassengers(passengers: Int)
+    fun uploadPassengers()
 
-    fun loadPassengers(passengers: Int) {
-        if (countOfPassengers + passengers > maxCountOfPassengers) {
-            println("Перегруз")
-        } else {
-            countOfPassengers += passengers
-            println("$countOfPassengers пассажиров загружено")
-        }
-    }
 }
 
 interface TruckingAble {
     var countOfCargo: Int
     val maxCountOfCargo: Int
-    fun loadCargo(cargo: Int) {
-        if (countOfCargo + cargo > maxCountOfCargo) {
-            println("Перегруз")
-        } else {
-            countOfCargo += cargo
-            println("$countOfCargo тонн груза загружено")
-        }
-    }
+    fun loadCargo(cargo: Int)
+    fun uploadCargo()
 }
 
 class CargoTruck() : MoveAble, PassengerTransportationAble, TruckingAble {
@@ -57,9 +50,54 @@ class CargoTruck() : MoveAble, PassengerTransportationAble, TruckingAble {
     override var countOfCargo: Int = 0
     override val maxCountOfPassengers: Int = 1
     override val maxCountOfCargo: Int = 2
+
+    override fun loadPassengers(passengers: Int) {
+        if (countOfPassengers + passengers > maxCountOfPassengers) {
+            println("Перегруз")
+        } else {
+            countOfPassengers += passengers
+            println("$countOfPassengers пассажиров загружено")
+        }
+    }
+
+    override fun uploadPassengers() {
+        println("$countOfPassengers пассажиров выгружено")
+        countOfPassengers = 0
+
+    }
+
+    override fun loadCargo(cargo: Int) {
+        if (countOfCargo + cargo > maxCountOfCargo) {
+            println("Перегруз")
+        } else {
+            countOfCargo += cargo
+            println("$countOfCargo тонн груза загружено")
+        }
+    }
+
+    override fun uploadCargo() {
+        println("$countOfCargo тонн груза выгружено")
+        countOfCargo = 0
+
+    }
 }
 
 class Car() : MoveAble, PassengerTransportationAble {
     override var countOfPassengers: Int = 0
     override val maxCountOfPassengers: Int = 3
+
+    override fun loadPassengers(passengers: Int) {
+        if (countOfPassengers + passengers > maxCountOfPassengers) {
+            println("Перегруз")
+        } else {
+            countOfPassengers += passengers
+            println("$countOfPassengers пассажиров загружено")
+        }
+    }
+
+    override fun uploadPassengers() {
+        println("$countOfPassengers пассажиров выгружено")
+        countOfPassengers = 0
+
+    }
 }
