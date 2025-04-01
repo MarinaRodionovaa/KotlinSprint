bd2ba721db18e7a65882c8a8b07a99fa83533785
marina_rodionova <marina_rodionova@>
2025-04-01 23:03:30 +0700

    KS-17-3 folder

---
+++ src/main/kotlin/Lesson_17/lesson17_task3.kt
@@ -0,0 +1,31 @@
+package org.example.Lesson_17
+
+fun main() {
+    val folder = Folder("folder", 12)
+    println("Имя папки: ${folder.name}\nКол-во файлов ${folder.countOfFiles}")
+    folder.isSecret = true
+    println()
+    println("Имя папки: ${folder.name}\nКол-во файлов ${folder.countOfFiles}")
+}
+
+class Folder(name: String, countOfFiles: Int) {
+    val name: String = name
+        get() {
+            if (isSecret) {
+                return "скрытая папка"
+            } else {
+                return field
+            }
+        }
+    val countOfFiles: Int = countOfFiles
+        get() {
+            if (isSecret) {
+                return 0
+            } else {
+                return field
+            }
+        }
+    var isSecret: Boolean = false
+
+
+}
