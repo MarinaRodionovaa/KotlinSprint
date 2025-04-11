5a5adc7a8e945108532203d14e4aa7fd22f8bf13
marina_rodionova <marina_rodionova@>
2025-04-11 12:27:46 +0500

    KS-20-2

--- src/main/kotlin/Lesson_20/lesson20_task2.kt
+++ src/main/kotlin/Lesson_20/lesson20_task2.kt
@@ -1,15 +1,15 @@
 package org.example.Lesson_20
 
 fun main() {
-    val heal: (it: Gamer) -> Unit = { it.currentHealth = it.maxHealth }
+    val heal: (it: Gamer1) -> Unit = { it.currentHealth = it.maxHealth }
 
-    val gamer = Gamer("Marina", 50)
+    val gamer = Gamer1("Marina", 50)
     gamer.printInfo()
     heal(gamer)
     gamer.printInfo()
 }
 
-class Gamer(name: String, var currentHealth: Int) {
+class Gamer1(name: String, var currentHealth: Int) {
     val maxHealth: Int = 100
     val printInfo = { println("Имя: $name, Текущее здоровье: $currentHealth, Максимальное здоровье: $maxHealth") }
 }
